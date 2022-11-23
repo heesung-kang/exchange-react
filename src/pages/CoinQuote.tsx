62f82061cc4133d147f52cf6f8bb41f87c0f2c03
kyle <kyle@addtune.net>
2022-11-23 15:49:07 +0900

    폰트 수정

--- src/pages/CoinQuote.tsx
+++ src/pages/CoinQuote.tsx
@@ -11,8 +11,7 @@ const CoinQuote: FunctionComponent = (): JSX.Element => {
         <h1 className={styles.title}>구매하실 코인을 선택하세요</h1>
         <div className={styles.comment}>
           국내 가상화폐 거래소의 시세를 반영한 구매 가격입니다.
-          <br /> 구매 시점과의 시차로 인해 실구매 시의 가격과는 다소 차이가 <br />
-          있을 수 있습니다.
+          <br /> 구매 시점과의 시차로 인해 실구매 시의 가격과는 다소 차이가 있을 수 있습니다.
         </div>
         <div className={styles.header}>
           <span>코인명/심볼</span>
